f7bb4c9ebe7d34781fb0d136a5019bd7ececef9b
Saraad <saraad@163.com>
2018-11-15 02:11:36 +0800

    com.freemud.demo.demo.java

---
+++ mydemo/src/main/java/com/freemud/demo/demo.java
@@ -0,0 +1,16 @@
+package com.freemud.demo;
+
+/**
+ * All rights Reserved, Designed By SARAAD
+ *
+ * @version V1.0
+ * @Title: demo
+ * @Package:com.freemud.demo
+ * @Description:
+ * @author: saraad
+ * @date: 2018/11/15 2:11
+ * @Copyright: 2018  Inc. All rights reserved.
+ * PROJECT FOR PRACTICE
+ */
+public class demo {
+}
